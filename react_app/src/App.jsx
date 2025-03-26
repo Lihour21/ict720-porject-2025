13b7138e07f12c4749ab763780bbb3808a9897d1
ChananyuKML <chananyukml@gmail.com>
2025-03-26 22:14:50 +0700

    Add multiple pages feature

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
+import StatisticPage from "./pages/StatisticPage";
+
+
 function App() {
-  return <div className='text-red-500 text-4xl bg-blue-500'>Hello World</div>;
+  return <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+    <Routes>
+      <Route path='/' element={<OverviewPage />} />
+      <Route path='/statistic' element={<StatisticPage />} />
+    </Routes>
+  </div>;
 }
 
 export default App;
