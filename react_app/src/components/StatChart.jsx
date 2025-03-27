c0cc0a1a4dcb273e04e9b14380418f67d142f87f
ChananyuKML <chananyukml@gmail.com>
2025-03-28 03:42:16 +0700

    Add Bar chart

---
+++ react_app/src/components/StatChart.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, BarChart, Bar  } from 'recharts'
+import { motion } from 'framer-motion'
+
+const ChartData = [
+    { name: "station 1", value: 100},
+    { name: "station 2", value: 80},
+    { name: "station 3", value: 90},
+    { name: "station 4", value: 60}
+]
+
+const StatChart = () => {
+  return (
+    <motion.div
+        ckkassName='bg-gray-800 bg-opacity-50 backdrop-blu-md overflow-hidden shadow-lg rounded-2xl p-6 border border-gray-700'
+        initial={{opacity: 1, y: 20}}
+        animate={{opacity: 1, y: 0}}
+        transition={{delay: 0.2}}>
+            <h2 className='text-lg font-medium mb-4 text-gray-100'>
+                Statistic Chart
+            </h2>
+            
+            <div className='h-80'>
+                <ResponsiveContainer width={"100%"} hight={"100%"}>
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={ChartData}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5
+                        }}
+                        barSize={20}
+                        >
+                        <CartesianGrid strokeDasharray='3 3' stoke='#4b5563'/>                
+                        <XAxis dataKey={"name"} stroke='#4b5563'/>
+                        <YAxis stroke='#9ca3af'/>
+                        <Tooltip/>
+
+                        <Bar dataKey="value"  fill="#8884d8" background={{ fill: "#eee", opacity: 0 }} />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+    </motion.div>
+  )
+}
+
+export default StatChart
--- react_app/src/pages/OverviewPage.jsx
+++ react_app/src/pages/OverviewPage.jsx
@@ -1,5 +1,7 @@
 import Header from "../components/common/Header";
 import StatCard from "../components/common/StatCard";
+import StatChart from "../components/StatChart";
+
 import { motion } from "framer-motion";
 import { Cpu, Smile, Annoyed, TriangleAlert  } from "lucide-react";
 
@@ -7,6 +9,7 @@ const OverviewPage = () => {
     return (<div className='flex-1 overflow-auto relative z-10'>
             <Header title="Overview" />
             <main className='max-w-7x1 mx-auto py-6 px-4 lg:px-8'>
+                {/* StatCard */}
                 <motion.div className='grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8'
                 initial={{ opacity: 0, y: 20}}
                 animate={{ opacity: 1, y:0}}
@@ -17,6 +20,12 @@ const OverviewPage = () => {
                     <StatCard name="Low" icon={Annoyed} value='39' color='#f7dc6f' />
                     <StatCard name="Very Low" icon={TriangleAlert} value='10' color='#e74c3c' />
                 </motion.div>
+                
+                {/* Charts */}
+
+                <div className='grid grid-cols-1 lg:grid-cols-2 lg:grid-flow-row gap-8'>
+                    <StatChart/>
+                </div>
             
             </main>
     </div>
