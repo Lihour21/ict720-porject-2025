f47304c64a5bd8c7dab8f692b22ac563e224199f
ChananyuKML <chananyukml@gmail.com>
2025-03-27 22:16:52 +0700

    edit Sidebar

--- react_app/src/components/Sidebar.jsx
+++ react_app/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
-import { BarChart2, DollarSign,Settings } from "lucide-react";
+import { BarChart2, DollarSign, Settings, Menu } from "lucide-react";
 import { useState } from 'react';
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 const SIDEBAR_ITEMS = [
     {
@@ -14,12 +14,20 @@ const Sidebar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   return (
     <motion.div 
-    className={`relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 ${
-      isSidebarOpen ? 'w-64' : 'w-20'}`}
-      animate={{ width: isSidebarOpen ? 256 : 80}}
-      >
-    <div className='h-full bg-gray-800 bg-opacity-50 backdrop-blur-md p-4 flex flex-col border-r border-gray-700'>
-    </div>
+        className={`relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 ${
+            isSidebarOpen ? "w-64" : "w-20"
+        }`}
+        animate={{ width: isSidebarOpen ? 256 : 80}}
+    >
+        <div className='h-full bg-gray-800 bg-opacity-50 backdrop-blur-md p-4 flex flex-col'>
+        <motion.button
+            whileHover={{ scale: 1.1}}
+            whileTap={{ scale: 0.9}}
+            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+            className='p-2 rounded-full hover:bg-gray-700 transition-colors max-w-fit'>
+            <Menu size={24}/>
+        </motion.button>
+        </div>
     </motion.div>
   );
 };
